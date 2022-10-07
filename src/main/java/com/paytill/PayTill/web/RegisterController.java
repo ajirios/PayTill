bf724dd9393d317b334fb959984a870ef2659c19
AJIRI OSAUZO JEFFREY <ajirios@outlook.com>
2022-10-07 14:35:55 -0500

    added register page

--- src/main/java/com/paytill/PayTill/web/RegisterController.java
+++ src/main/java/com/paytill/PayTill/web/RegisterController.java
@@ -1,10 +1,25 @@
 package com.paytill.PayTill.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController 
 
 {
+	@GetMapping("/register")
+	public String getRegister(ModelMap modelMap)
 	
+	{
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String postRegister(ModelMap modelMap)
+	
+	{
+		return "redirect:/register";
+	}
 }
